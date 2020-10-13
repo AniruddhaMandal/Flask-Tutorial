7300cca404d981e6cbe9d76dcf04c060294f3f60
Aniruddha Mandal <ani96dh@gmail.com>
2020-10-13 21:31:26 +0530

    Erros Fixed

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -1,14 +1,14 @@
 import  functools
 from flask import (Blueprint, flash, g, redirect, render_template, request, session,url_for)
-from werkzeug.security import check_passeword_hash, generate_password_hash
+from werkzeug.security import check_password_hash, generate_password_hash
 
-from flask.db import get_db
+from flaskr.db import get_db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 @bp.route('/register', methods =('GET', 'POST'))
 def register():
-	if equest.method == 'POST':
+	if request.method == 'POST':
 		username = request.form['username']
 		password = request.form['password']
 		db = get_db()
@@ -31,9 +31,9 @@ def register():
 		flash(error)
 	return render_template('auth/register.html')
 
-@bp.route('/login', method('GET', 'POST'))
+@bp.route('/login', methods = ('GET', 'POST'))
 def login():
-	if request.method ='POST':
+	if request.method == 'POST':
 		username = request.form['username']
 		password = request.form['password']
 		db = get_db()
